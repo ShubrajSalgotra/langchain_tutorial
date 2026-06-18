4946061ac2becb80803da639731b1f2c6a7d8a7d
Arnav Jain <arnavjain180@gmail.com>
2026-06-18 22:15:53 +0530

    Added Chat History Functionality and Cookie Management

--- flaskChatBot/app.py
+++ flaskChatBot/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request, render_template
-from langchain.messages import SystemMessage, HumanMessage
+from flask import Flask, jsonify, request, render_template, session
+from langchain.messages import SystemMessage, HumanMessage, AIMessage
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 import os
@@ -11,9 +11,13 @@ model = init_chat_model("google_genai:gemini-2.5-flash-lite", api_key=api_key)
 
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SESSION_SECRET_KEY")
 
 @app.route("/")
 def home():
+    if "chatHistory" not in session:
+        session["chatHistory"] = []
+    print(session)
     return render_template("index.html")
 
 @app.route("/chat", methods=["POST"])
@@ -23,14 +27,37 @@ def chat():
     if not message:
         return jsonify({"Error": "Message not found"})
     
+    if "chatHistory" not in session:
+        session["chatHistory"] = []
+
     messageChain = [
         SystemMessage("You are helpful chatbot."),
-        HumanMessage(message),
     ]
 
+    for chat in session["chatHistory"]:
+        if chat["role"] == "user":
+            messageChain.append(HumanMessage(chat["content"]))
+        else:
+            messageChain.append(AIMessage(chat["content"]))
+
+    messageChain.append(HumanMessage(message))
+
     response = model.invoke(messageChain)
+    session["chatHistory"].append({"role": "user", "content": message})
+    session["chatHistory"].append({"role": "AI", "content": response.content})
+
+    session.modified = True
+    print(session)
 
     return jsonify({"Reply": response.content})
+
+@app.route("/clear", methods=["POST"])
+def clear():
+    session["chatHistory"] = []
+    session.modified = True
+
+    return jsonify({"Message": "Chat Cleared"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
